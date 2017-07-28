e5ac94463b33dbc2d0f5c14440075a9d164e2303
Erica Rottman <erica7650@gmail.com>
2017-07-28 07:41:15 -0700

    added mocha/chai testing npm init

---
+++ test/loop-test.js
@@ -0,0 +1,10 @@
+var chai = require('chai')
+var assert = chai.assert
+var every = require('../src/loopSolution')
+
+// describe('every', function () {
+//   it('should return action path of unordered array', function () {
+//     var input = ["project", "client", "lint"]
+//     assert.equal(every(input, callback), input.every(callback))
+//   })
+// })
